58ab20ff709e05f770dee1563116fbfdd27e0a15
luis-jmtz <luis.jmtz2002@gmail.com>
2025-09-09 22:18:27 -0500

    replace text for markdown works

--- Functs.py
+++ Functs.py
@@ -1,12 +1,16 @@
 import markdown
 
-def text_replace_md(file_path):
+def text_replace_md(file_path, find_text, replacement_text, new_file):
 
     with open(file_path, "r", encoding="utf-8") as md_file:
         text = md_file.read()
 
+    # print(text)
+    text = text.replace(find_text, replacement_text)
+
+    with open(new_file, 'w') as file:
+        file.write(text)
+
     print(text)
 
-    return 0
-
-text_replace_md(r"Uncleaned_Files\Barbarian.md")
+text_replace_md(r"Uncleaned_Files\Barbarian.md", "Barbarian", "Thug", "test2.md")
